c933c8adf7eedb63a4955bcc09853f77078f79da
Florian Pürschel <hello@prshl.de>
2022-01-29 11:40:11 +0100

    add def props to autocomplete

--- src/autocomplete/index.tsx
+++ src/autocomplete/index.tsx
@@ -1,5 +1,13 @@
 import { Base } from 'base';
+import { CursorType } from 'enums/cursor';
+import { AutocompleteProps } from './interface';
 
-export const Autocomplete = () => {
-	return <Base />;
+export const Autocomplete = (props: AutocompleteProps) => {
+	return <Base cursor={props.cursor} placeholder={props.placeholder} fullWidth={props.fullWidth} />;
+};
+
+Autocomplete.defaultProps = {
+	cursor: CursorType.Text,
+	placeholder: 'Enter your placeholder',
+	fullWidth: false,
 };
